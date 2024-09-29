39206d72ccd3413296bf96b5ff68dac86953760b
realsticklabs <sinkfieldalex8@gmail.com>
2024-09-29 18:51:05 -0400

    Update primeWorker.js

--- primeWorker.js
+++ primeWorker.js
@@ -1,5 +1,5 @@
-let primes = [2];
-let currentNum = 3;
+let primes = JSON.parse(localStorage.getItem('primes')) || [2];
+let currentNum = primes.length > 1 ? primes[primes.length - 1] + 2 : 3; // Start checking from the next odd number after the last found prime
 
 function isPrime(num) {
     for (let i = 2; i <= Math.sqrt(num); i++) {
@@ -9,11 +9,15 @@ function isPrime(num) {
 }
 
 function findNextPrime() {
-    if (isPrime(currentNum)) {
-        primes.push(currentNum);
-        postMessage({ highestPrime: currentNum, totalPrimes: primes.length });
+    while (true) {
+        if (isPrime(currentNum)) {
+            primes.push(currentNum);
+            localStorage.setItem('primes', JSON.stringify(primes)); // Save to localStorage
+            postMessage({ highestPrime: currentNum, totalPrimes: primes.length });
+            break; // Exit after finding the next prime
+        }
+        currentNum += 2; // Check only odd numbers
     }
-    currentNum += 2; // Check only odd numbers
 }
 
 // Check for new primes every 33 milliseconds
